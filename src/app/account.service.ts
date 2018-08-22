72439f079450004e0f051841425ba41aa9064014
Kevin W <rocwang0316@gmail.com>
2018-08-22 13:31:42 -0700

    Added the getBalance function to the account service.

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { FETCH_ALL_ACCOUNT_API } from '../UrlApis';
+import { FETCH_ALL_ACCOUNT_API, ADD_USER_API } from '../UrlApis';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,13 @@ export class AccountService {
 
   fetchAllAcount(): Observable<object []> {
     return this.http.get<Object []>(FETCH_ALL_ACCOUNT_API);
+  }
+
+  addUser(firstName: string, lastName: string, initailAmount: number): Observable<string> {
+    return this.http.post<string>(ADD_USER_API, { firstName, lastName, initailAmount });
+  }
+
+  fetchBalance(userId: string): Observable<string> {
+    return this.http.get<string>(userId);
   }
 }
