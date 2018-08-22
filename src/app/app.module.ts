183787a1c54e12e3aa90851ab657fc5f850b46b2
Kevin W <rocwang0316@gmail.com>
2018-08-22 11:02:14 -0700

    Added the angular material package.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AccountListComponent } from './account-list/account-list.component';
 import { AccountDetailComponent } from './account-detail/account-detail.component';
+import { AddAccountFormComponent } from './add-account-form/add-account-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountListComponent,
-    AccountDetailComponent
+    AccountDetailComponent,
+    AddAccountFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
